4fedab6f21787200e3e48bccabac4be4b97c366e
Red16 <71528727+Rednick16@users.noreply.github.com>
2023-05-07 00:48:46 -0500

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,9 +1,9 @@
 local utils = require "utils"
 
 local function main()
-    local loaderUI = utils.setupUI("Rednick16", "Extra Settings for Terraria", "Please no more terraria")
+    local loaderUI, mainView = utils.setupUI("Rednick16", "Extra Settings for Terraria", "Please no more terraria")
     
-    local section = utils.addSection(loaderUI, "God hacks", "Turn them off")
+    local section = utils.addSection(mainView, "God hacks", "Turn them off")
 
     utils.addSwitch(section, "OkSwitch", "Godmode", 1, "Turn this on you won't die")
     
@@ -11,18 +11,21 @@ local function main()
     
     utils.addSlider(section, "testSlider", "Slider S", 40, "", -100, 100)
     
-    utils.addPickerView(section, "pickerViewKey", "picker view", "", 0, {"Man", "Woman", "Dog", "Children"})
+    utils.addPickerView(section, "pickerViewKey",
+    "picker view", "", 0, {
+        {"Man", "Woman", "Dog", "Children"}
+    })
     
+    local section2 = utils.addSection(mainView, "Section 2", "")
+    utils.addPickerView(section2, "pickerViewKey2",
+    "picker view", "", 0, {
+        {"Test", "Ok", "No", "Idiot", "okay bob"},
+        {"Nop", "RET", "MOV X", "MOV W", "BL"}
+    })
     
-    local section2 = utils.addSection(loaderUI, "Section 2", "")
-    utils.addPickerView(section2, "pickerViewKey", "picker view", "", 0, {"Test", "Ok", "No", "Idiot", "okay bob"})
-    
-    print(getValueForKey("OkSwitch"))
-    
-    -- Creating a navagtion switch(Not ready yet :(
     local navItem = utils.addNavigationSwitch(section, "navKey_827", "Nav Switch", "Takes you to a new page.")
-    --local navSection = utils.addSection(navItem, "God hacks", "Turn them off")
-    --utils.addSwitch(navItem, "ND", "No Drown", 1, "Turn this on you won't drown")
+    local navSection = utils.addSection(navItem, "navItem section", "navItem description")
+    utils.addSwitch(navSection, "ND", "No Drown", 1, "Turn this on you won't drown")
 
     -- Don't touch this.
     au.addSection(loaderUI)
